13a7cbb9eca0de7f026bd42a416fa213f8882bcd
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-28 16:28:29 +0200

    Add methods for Temporal values
    TOML support DateTime values, that's why I added methods for them in TomlConfiguration. I used a TemporalAccessor because a DateTime may be a LocalDateTime, ZonedDateTime or LocalDate. (this is not supported by all Toml libraries for now, at least by mine).

--- src/org/mcphoton/config/TomlConfiguration.java
+++ src/org/mcphoton/config/TomlConfiguration.java
@@ -3,6 +3,7 @@ package org.mcphoton.config;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.temporal.TemporalAccessor;
 import java.util.Map;
 
 public class TomlConfiguration extends BaseConfiguration {
@@ -13,6 +14,26 @@ public class TomlConfiguration extends BaseConfiguration {
 	
 	public TomlConfiguration(Map<String, Object> map) {
 		// defined by the photon's implementation
+	}
+	
+	/**
+	 * Checks if the key exists and refers to a {@link TemporalAccessor} (basically Date or DateTime) value.
+	 * 
+	 * @param key the key key the key (may be compound)
+	 * @return true if the key refers to a Date or DateTime
+	 */
+	public boolean containsTemporal(String key) {
+		return get(key) instanceof TemporalAccessor;
+	}
+	
+	/**
+	 * Gets the {@link TemporalAccessor} value mapped to this key.
+	 * 
+	 * @param key the key (may be compound)
+	 * @return the mapped value, converted to a TemporalAccessor.
+	 */
+	public TemporalAccessor getTemporal(String key) {
+		return (TemporalAccessor) get(key);
 	}
 	
 	@Override
