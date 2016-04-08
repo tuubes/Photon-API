8ac2f4609f5f359e893ab6ec1e3a0356495e0fd8
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-08 19:31:44 +0200

    documentation

--- src/main/java/org/mcphoton/Photon.java
+++ src/main/java/org/mcphoton/Photon.java
@@ -11,70 +11,70 @@ import org.mcphoton.plugin.PluginsManager;
 import org.mcphoton.world.BiomeRegistry;
 
 /**
- * The centralized API core. Gives access to many core points of the API, like the {@link PluginsManager} and the
- * {@link EventManager}.
- * 
+ * The centralized API core. Gives access to many core points of the API, like the {@link PluginsManager} and
+ * the {@link EventManager}.
+ *
  * @author TheElectronWill
- * 		
+ *
  */
 public final class Photon {
-	
+
 	private Photon() {}
-	
+
 	public static PluginsManager pluginsManager() {
 		return null;
 	}
-	
+
 	public static EventsManager eventsManager() {
 		return null;
 	}
-	
+
 	public static EntityRegistry entityRegistry() {
 		return null;
 	}
-	
+
 	public static BlockRegistry blockRegistry() {
 		return null;
 	}
-	
+
 	public static ItemRegistry itemRegistry() {
 		return null;
 	}
-	
+
 	public static BiomeRegistry biomeRegistry() {
 		return null;
 	}
-	
+
 	public static PacketRegistry packetRegistry() {
 		return null;
 	}
-	
+
 	public static PacketService packetService() {
 		return null;
 	}
-	
+
 	public static boolean isClient() {
 		return false;
 	}
-	
+
 	public static boolean isServer() {
 		return false;
 	}
-	
+
 	public static String version() {
 		return "0.1.1-alpha";
 	}
-	
+
 	public static String minecraftVersion() {
 		return "1.9";
 	}
-	
+
 	public static File mainDirectory() {
 		return null;
 	}
-	
+
 	public static File pluginsDirectory() {
 		return null;
 	}
-	
+
 }
--- src/main/java/org/mcphoton/world/Location.java
+++ src/main/java/org/mcphoton/world/Location.java
@@ -1,64 +1,77 @@
 package org.mcphoton.world;
 
 /**
- * A location in a World, as 3 coordinates (x,y,z). Location is immutable. The methods "with..." create a new Location
- * object.
- * 
+ * A location is defined by 3 coordinates (x,y,z) and one World. A Location is immutable, so thread-safe. The
+ * methods {@link #withX(double)}, {@link #withY(double)}, {@link #withZ(double)} and
+ * {@link #withWorld(World)} create a new Location object.
+ *
  * @author TheElectronWill
- * 		
+ *
  */
 public final class Location {
-	
+
 	private final double x, y, z;
 	private final World w;
-	
+
 	public Location(double x, double y, double z, World w) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.w = w;
 	}
-	
+
 	public World getWorld() {
 		return w;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	public double getZ() {
 		return z;
 	}
-	
+
 	public int getBlockX() {
 		return (int) x;
 	}
-	
+
 	public int getBlockY() {
 		return (int) y;
 	}
-	
+
 	public int getBlockZ() {
 		return (int) z;
 	}
-	
+
+	/**
+	 * Creates a new Location with the specified x coordinate and the same y, z and World as this Location.
+	 */
 	public Location withX(double x) {
 		return new Location(x, y, z, w);
 	}
-	
+
+	/**
+	 * Creates a new Location with the specified y coordinate and the same x, z and World as this Location.
+	 */
 	public Location withY(double y) {
 		return new Location(x, y, z, w);
 	}
-	
+
+	/**
+	 * Creates a new Location with the specified z coordinate and the same x, y and World as this Location.
+	 */
 	public Location withZ(double z) {
 		return new Location(x, y, z, w);
 	}
-	
+
+	/**
+	 * Creates a new Location with the specified world and the same x, y and z as this Location.
+	 */
 	public Location withWorld(World w) {
 		return new Location(x, y, z, w);
 	}
