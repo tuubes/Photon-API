f7e91d038a735b9ff839f331c09c339704b8cc81
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-08 19:33:37 +0200

    Use ChunkColumn and ChunkSection + remove methods about light level
    I will add the methods later. For now I need to focus on important features, like blocks, not light level.

--- src/main/java/org/mcphoton/world/World.java
+++ src/main/java/org/mcphoton/world/World.java
@@ -8,48 +8,50 @@ import org.mcphoton.world.protection.UnlockedAreaAccess;
 import org.mcphoton.world.protection.WorldAccess;
 import org.mcphoton.world.protection.WorldAccessManager;
 
+/**
+ * A game world.
+ *
+ * @author TheElectronWill
+ */
 public interface World {
-	
+
 	// === Properties ===
-	
 	String getName();
-	
+
 	void renameTo(String name);
-	
+
 	File getDirectory();
-	
+
 	int getType();
-	
+
 	Location getSpawn();
-	
+
 	void setSpawn(int x, int y, int z);
-	
+
 	void setSpawn(Location spawn);
-	
+
 	// === Misc ===
-	
 	Collection<Player> getPlayers();
-	
+
 	void save();
-	
+
 	void delete();
-	
+
 	// === Block Access ===
-	
 	WorldAccessManager getAccessManager();
-	
+
 	void setAccessManager(WorldAccessManager manager);
-	
+
 	default WorldAccess access() {
 		return getAccessManager().getAccess();
 	}
-	
+
 	default Optional<UnlockedAreaAccess> accessArea(Area area, Object accessor) {
 		return getAccessManager().unlockArea(area, accessor);
 	}
-	
-	default Optional<Chunk> accessChunk(int x, int z, Object accessor) {
+
+	default Optional<ChunkColumn> accessChunk(int x, int z, Object accessor) {
 		return getAccessManager().unlockChunk(x, z, accessor);
 	}
-	
+
 }
--- src/main/java/org/mcphoton/world/protection/UnlockedAreaAccess.java
+++ src/main/java/org/mcphoton/world/protection/UnlockedAreaAccess.java
@@ -6,41 +6,29 @@ import org.mcphoton.world.BiomeType;
 import org.mcphoton.world.Location;
 
 public interface UnlockedAreaAccess extends ReadOnlyBlockAccess {
-	
+
 	Area getArea();
-	
+
 	boolean breakBlock(int x, int y, int z);
-	
+
 	default boolean breakBlock(Location loc) {
 		return breakBlock(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 	}
-	
+
 	boolean setBlockType(int x, int y, int z, BlockType type);
-	
+
 	default boolean setBlockType(Location loc, BlockType type) {
 		return setBlockType(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), type);
 	}
-	
-	boolean setBlockEmittedLight(int x, int y, int z, byte light);
-	
-	default boolean setBlockEmittedLight(Location loc, byte light) {
-		return setBlockEmittedLight(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), light);
-	}
-	
-	boolean setBlockSkylight(int x, int y, int z, byte skylight);
-	
-	default boolean setBlockSkylight(Location loc, byte skylight) {
-		return setBlockSkylight(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), skylight);
-	}
-	
+
 	boolean setBiomeType(int x, int y, int z, BiomeType type);
-	
+
 	default boolean setBiomeType(Location loc, BiomeType type) {
 		return setBiomeType(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), type);
 	}
-	
+
 	boolean fill(int blockId);
-	
+
 	boolean replaceAll(int toReplaceId, int replacementId);
-	
+
 }
