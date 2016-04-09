0723ea36c79ee9ef8a2a0fd749083cd2defa9f7c
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-09 15:51:10 +0200

    Formatting

--- src/main/java/org/mcphoton/entity/Entity.java
+++ src/main/java/org/mcphoton/entity/Entity.java
@@ -1,61 +1,61 @@
 package org.mcphoton.entity;
 
+import com.electronwill.utils.IntConstant;
 import org.mcphoton.network.ProtocolOutputStream;
 import org.mcphoton.world.World;
-import com.electronwill.utils.IntConstant;
 
 /**
- * Base class for entites. Entities normally don't need to be thread-safe, because the photon's implementation take care
- * of this.
- * 
+ * Base class for entites. Entities normally don't need to be thread-safe, because the photon's implementation
+ * takes care of this.
+ *
  * @author TheElectronWill
- * 		
+ *
  */
 public abstract class Entity {
-	
+
 	private final IntConstant entityId = new IntConstant();
-	
+
 	protected String customName = "";
 	protected double x, y, z;
 	protected World world;
-	
+
 	public int getEntityId() {
 		return entityId.get();
 	}
-	
+
 	public void initializeEntityId(int id) {
 		this.entityId.init(id);
 	}
-	
+
 	public abstract EntityType getType();
-	
+
 	public String getCustomName() {
 		return customName;
 	}
-	
+
 	public void setCustomName(String customName) {
 		this.customName = customName;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	public double getZ() {
 		return z;
 	}
-	
+
 	public World getWorld() {
 		return world;
 	}
-	
+
 	/**
 	 * Writes this entity to a ProtocolOutputStream.
 	 */
 	public abstract void writeTo(ProtocolOutputStream out);
-	
+
 }
