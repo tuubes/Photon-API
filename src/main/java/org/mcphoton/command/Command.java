abead58d914522dba70c277dda4499d310c90a2a
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-13 17:29:38 +0200

    Split arguments

--- src/main/java/org/mcphoton/command/Command.java
+++ src/main/java/org/mcphoton/command/Command.java
@@ -1,11 +1,33 @@
 package org.mcphoton.command;
 
+import com.electronwill.utils.StringUtils;
+import java.util.List;
+
 /**
+ * A command that can be executed.
  *
  * @author TheElectronWill
  */
 public interface Command {
 
-	void execute(Object source, String args);
+	/**
+	 * Executes this command.
+	 *
+	 * @param source the person or thing that executes this command
+	 * @param argString the string that contains all the characters except the command name
+	 */
+	default void execute(Object source, String argString) {
+		List<String> parts = StringUtils.split(argString, ' ');
+		String[] array = new String[parts.size()];
+		execute(source, parts.toArray(array));
+	}
+
+	/**
+	 * Executes this command.
+	 *
+	 * @param source the person or thing that executes this command
+	 * @param args the array that contains the command's arguments
+	 */
+	void execute(Object source, String[] args);
 
 }
