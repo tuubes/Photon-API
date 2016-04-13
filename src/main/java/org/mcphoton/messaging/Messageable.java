39125c3640fb7e3129bb00cd50b4259c35a24949
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-13 14:58:01 +0200

    grammar fix in doc

--- src/main/java/org/mcphoton/messaging/Messageable.java
+++ src/main/java/org/mcphoton/messaging/Messageable.java
@@ -1,14 +1,14 @@
 package org.mcphoton.messaging;
 
 /**
- * Interface for objects which can receive messages.
- * 
+ * Interface for objects that can receive messages.
+ *
  * @author TheElectronWill
  */
 public interface Messageable {
-	
+
 	void sendMessage(CharSequence msg);
-	
+
 	void sendMessage(ChatMessage msg);
-	
+
 }
