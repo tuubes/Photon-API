473bc683db9c1cfca56f05c258932d0020928489
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-13 14:58:12 +0200

    Remove license header in source files

--- src/main/java/org/mcphoton/messaging/Color.java
+++ src/main/java/org/mcphoton/messaging/Color.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2015 ElectronWill
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- */
 package org.mcphoton.messaging;
 
 /**
@@ -19,7 +5,7 @@ package org.mcphoton.messaging;
  * @author TheElectronWill
  */
 public enum Color {
-	
+
 	/**
 	 * dark black.
 	 */
@@ -84,19 +70,19 @@ public enum Color {
 	 * light yellow.
 	 */
 	YELLOW("\u001B[33m");
-	
+
 	/**
 	 * The ansi code for printing this color in a terminal (works well on Mac, Linux and Solaris).
 	 */
 	public final String ansi;
-	
+
 	private Color(String ansi) {
 		this.ansi = ansi;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name().toLowerCase();
 	}
-	
+
 }
