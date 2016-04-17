ed40a95c0a91e9d47ecb7dfce76221c521956a4b
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-17 12:20:12 +0200

    Remove useless IOException

--- src/main/java/org/mcphoton/network/ProtocolWriteable.java
+++ src/main/java/org/mcphoton/network/ProtocolWriteable.java
@@ -1,24 +1,22 @@
-/* 
+/*
  * Copyright (c) 2016 MCPhoton <http://mcphoton.org> and contributors.
- * 
+ *
  * This file is part of the Photon API <https://github.com/mcphoton/Photon-API>.
- * 
+ *
  * The Photon API is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The Photon API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.mcphoton.network;
-
-import java.io.IOException;
 
 /**
  * Something that can be written to a ProtocolOutputStream.
@@ -26,13 +24,13 @@ import java.io.IOException;
  * @author TheElectronWill
  */
 public interface ProtocolWriteable {
-	
+
 	/**
 	 * Writes this object to a ProtocolOutputStream.
 	 *
 	 * @param out the output stream to write this object to.
 	 * @throws java.io.IOException if a problem occurs
 	 */
-	void writeTo(ProtocolOutputStream out) throws IOException;
-	
+	void writeTo(ProtocolOutputStream out);
+
 }
