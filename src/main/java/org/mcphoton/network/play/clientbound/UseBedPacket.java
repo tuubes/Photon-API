4d698ab4092e0b93116068578271c2c0600e59db
Maaattt <matthias.ehrenfeld@gmail.com>
2016-04-18 19:12:53 +0200

    Add DestroyEntities, RemoveEntityEffect and UseBed packets

---
+++ src/main/java/org/mcphoton/network/play/clientbound/UseBedPacket.java
@@ -0,0 +1,63 @@
+/*
+* Copyright (c) 2016 MCPhoton <http://mcphoton.org> and contributors.
+*
+* This file is part of the Photon API <https://github.com/mcphoton/Photon-API>.
+*
+* The Photon API is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* The Photon API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.mcphoton.network.play.clientbound;
+
+import org.mcphoton.network.Packet;
+import org.mcphoton.network.ProtocolHelper;
+import org.mcphoton.network.ProtocolOutputStream;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author Maaattt
+ */
+public class UseBedPacket implements Packet {
+
+    public int entityId;
+    public int x,y,z;
+
+
+    @Override
+    public int getId() { return 0x2F; }
+
+    @Override
+    public boolean isServerBound() { return false; }
+
+    @Override
+    public void writeTo(ProtocolOutputStream out) {
+        out.writeVarInt(entityId);
+        out.writeLong(ProtocolHelper.encodePosition(x,y,z));
+
+    }
+
+    @Override
+    public Packet readFrom(ByteBuffer buff) {
+        entityId = ProtocolHelper.readVarInt(buff);
+        x = ProtocolHelper.decodePositionX(buff.getLong());
+        y = ProtocolHelper.decodePositionY(buff.getLong());
+        z = ProtocolHelper.decodePositionZ(buff.getLong());
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "UseBedPacket{" + "entityId=" + entityId + ", x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+}
