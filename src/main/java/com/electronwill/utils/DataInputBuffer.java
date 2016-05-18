ebd848a799b52f0f0d6b1a0b941dbd1cbf0a56a5
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-05-18 18:31:20 +0200

    Fix license header

--- src/main/java/com/electronwill/utils/DataInputBuffer.java
+++ src/main/java/com/electronwill/utils/DataInputBuffer.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2016 MCPhoton <http://mcphoton.org> and contributors.
  *
- * This file is part of the Photon Server Implementation <https://github.com/mcphoton/Photon-Server>.
+ * This file is part of the Photon API <https://github.com/mcphoton/Photon-API>.
  *
- * The Photon Server Implementation is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * The Photon API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The Photon Server Implementation is distributed in the hope that it will be useful,
+ * The Photon API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.electronwill.utils;
