5dfc3cfe8d0751db694cbad7f00af773a513f8ef
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-06-10 23:27:15 +0200

    Add a constructor to simplify the creation of SimpleBag from a single value

--- src/main/java/com/electronwill/utils/SimpleBag.java
+++ src/main/java/com/electronwill/utils/SimpleBag.java
@@ -1,18 +1,18 @@
-/* 
+/*
  * Copyright (c) 2016 MCPhoton <http://mcphoton.org> and contributors.
- * 
+ *
  * This file is part of the Photon API <https://github.com/mcphoton/Photon-API>.
- * 
+ *
  * The Photon API is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The Photon API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -81,6 +81,16 @@ public class SimpleBag<E> extends AbstractCollection<E> implements Bag<E> {
 	public SimpleBag(int initialCapacity, int capacityIncrement) {
 		array = new Object[initialCapacity];
 		this.capacityIncrement = capacityIncrement;
+	}
+
+	public SimpleBag(Object firstElement) {
+		this(10, 2);
+		array[size++] = firstElement;
+	}
+
+	public SimpleBag(Object firstElement, int initialCapacity, int capacityIncrement) {
+		this(initialCapacity, capacityIncrement);
+		array[size++] = firstElement;
 	}
 
 	@Override
