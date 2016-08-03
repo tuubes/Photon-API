a9f35b7c9b24f4278861acde265c6073dcb3b3bf
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-08-03 12:17:14 +0200

    Add a CubicArea + move areas in a specific package

--- src/main/java/org/mcphoton/world/areas/Area.java
+++ src/main/java/org/mcphoton/world/areas/Area.java
@@ -16,18 +16,38 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.mcphoton.world;
+package org.mcphoton.world.areas;
 
+import org.mcphoton.world.Location;
+import org.mcphoton.world.World;
+
+/**
+ * A 3D area that contains blocks.
+ *
+ * @author TheElectronWill
+ */
 public interface Area extends Iterable<Location> {
 
+	/**
+	 * Checks if the given coordinates are inside this area.
+	 */
 	boolean contains(int x, int y, int z);
 
+	/**
+	 * Checks if the given location is inside this area.
+	 */
 	default boolean contains(Location loc) {
 		return loc.getWorld() == getWorld() && contains(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 	}
 
+	/**
+	 * Gets the area's world.
+	 */
 	World getWorld();
 
+	/**
+	 * Gets the area's size, in blocks (empty blocks are counted).
+	 */
 	int size();
 
 }
