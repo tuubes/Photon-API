56527f28938e1cb7dce6adcd2f45088c149357f9
TheElectronWill <theelectronwill@gmail.com>
2016-11-12 01:59:34 +0100

    Implement ConfigSpecification's correction and checking

--- src/main/java/org/mcphoton/config/ConfigSpecification.java
+++ src/main/java/org/mcphoton/config/ConfigSpecification.java
@@ -1,7 +1,6 @@
 package org.mcphoton.config;
 
 import java.util.*;
-import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 /**
@@ -190,9 +189,8 @@ public class ConfigSpecification {
 		ValueSpecification spec = map.get(path);
 		if (spec == null) {
 			return false;
-		} else {
-			return spec.validator.test(value);
 		}
+		return spec.validator.test(value);
 	}
 
 	/**
@@ -202,19 +200,104 @@ public class ConfigSpecification {
 	 * @return {@code true} if the config is correct, {@code false} if it isn't.
 	 */
 	public boolean check(Config config) {
-		//TODO
+		return check(config, (r, p, v) -> {}, true);
 	}
 
 	/**
-	 * Checks if the given config complies with this specification. Calls the {@code rejectedValueConsumer}
-	 * for each config value that does not complies with this specification.
+	 * Checks if the given config complies with this specification. Calls the {@code listener}
+	 * for each config value that does not complies with this specification, and for each missing config
+	 * value.
 	 *
-	 * @param config                the config to check.
-	 * @param rejectedValueConsumer the consumer to use for each incorrect config value.
+	 * @param config           the config to check.
+	 * @param listener         the listener to call for each incorrect (or missing, or unwanted) config value.
+	 * @param stopAfterOneFail {@code true} to stop after one check fails, {@code false} to continue and
+	 *                         check everything.
 	 * @return {@code true} if the config is correct, {@code false} if it isn't.
 	 */
-	public boolean check(Config config, BiConsumer<String, Object> rejectedValueConsumer) {
-		//TODO
+	public boolean check(Config config, CheckListener listener, boolean stopAfterOneFail) {
+		// Step 1 - check for incorrect or unwanted config values
+		boolean valid = checkValues(config, listener, stopAfterOneFail, "");//recursive
+
+		// Step 2 - check for missing values
+		for (Map.Entry<String, ValueSpecification> specEntry : map.entrySet()) {
+			String path = specEntry.getKey();
+			ValueSpecification spec = specEntry.getValue();
+			if (!config.containsValue(path)) {
+				listener.onCheckFailed(CheckFailReason.MISSING, path, null);
+				if (stopAfterOneFail)
+					return false;
+			}
+		}
+		return valid;
+	}
+
+	private boolean checkValues(Config config, CheckListener listener, boolean stopAfterOneFail, String configPath) {
+		boolean valid = true;
+		Set<Map.Entry<String, Object>> configEntries = config.asMap().entrySet();
+		for (Iterator<Map.Entry<String, Object>> it = configEntries.iterator(); it.hasNext(); ) {
+			Map.Entry<String, Object> configEntry = it.next();
+			String key = configEntry.getKey();
+			Object value = configEntry.getValue();
+
+			String path = (configPath.isEmpty()) ? key : configPath + '.' + key;
+			ValueSpecification spec = map.get(path);
+
+			if (value instanceof Config) {
+				valid &= checkValues(config, listener, stopAfterOneFail, path);//process the content of the config
+				if (stopAfterOneFail && !valid) {
+					return false;
+				}
+			} else {
+				if (spec == null) {//no specification = unwanted value
+					listener.onCheckFailed(CheckFailReason.UNWANTED, path, value);
+					it.remove();
+					if (stopAfterOneFail) {
+						return false;
+					}
+					valid = false;
+				} else if (!spec.validator.test(value)) {//incorrect value
+					listener.onCheckFailed(CheckFailReason.INCORRECT_VALUE, path, value);
+					if (stopAfterOneFail) {
+						return false;
+					}
+					valid = false;
+				}
+			}
+		}
+		return valid;
+	}
+
+	/**
+	 * Listen for the checks made by the methods {@link #check(Config)} and
+	 * {@link #check(Config, CheckListener, boolean)}.
+	 */
+	@FunctionalInterface
+	public static interface CheckListener {
+		/**
+		 * Called when a config check fail, that is, when the {@code check} method detects that a
+		 * config value is missing, is incorrect or is unwanted.
+		 *
+		 * @param reason the reason of the failure.
+		 * @param path   the path of the value.
+		 * @param value  the config value. May be null.
+		 */
+		void onCheckFailed(CheckFailReason reason, String path, Object value);
+	}
+
+	public static enum CheckFailReason {
+		/**
+		 * Means that a value is missing for this path, that is, it should be added to the config. In
+		 * that case, {@code value} is always {@code null}.
+		 */
+		MISSING,
+		/**
+		 * Means that the value is incorrect, that is, it doesn't conform to the specification).
+		 */
+		INCORRECT_VALUE,
+		/**
+		 * Means that the value is unwanted, that is, it should be removed from the config.
+		 */
+		UNWANTED
 	}
 
 	/**
@@ -228,33 +311,98 @@ public class ConfigSpecification {
 	 * @return the number of corrected values.
 	 */
 	public int correct(Config config) {
-		return correct(config, (p, iv, cv) -> {});
+		return correct(config, (a, p, i, c) -> {});
 	}
 
 	/**
 	 * Corrects the given config so that it complies with this spefication. Call the {@code
-	 * correctedValueAction} for each config value that is corrected, added or removed.
+	 * listener} for each config value that is added, replaced or removed.
 	 *
-	 * @param config               the config to correct.
-	 * @param correctedValueAction the action to do for each corrected config value.
+	 * @param config   the config to correct.
+	 * @param listener the action to do for each corrected config value.
 	 * @return the number of corrected values.
 	 */
-	public int correct(Config config, CorrectionReaction correctedValueAction) {
-		//TODO
+	public int correct(Config config, CorrectionListener listener) {
+		// Step 1 - modify or remove the incorrect config values
+		int corrected = correctBadValues(config, listener, "");//recursive
+
+		// Step 2 - add the missing values to the config
+		for (Map.Entry<String, ValueSpecification> specEntry : map.entrySet()) {
+			String path = specEntry.getKey();
+			ValueSpecification spec = specEntry.getValue();
+			if (!config.containsValue(path)) {
+				listener.onCorrect(CorrectionAction.ADD, path, null, spec.defaultValue);
+				config.setValue(path, spec.defaultValue);
+				corrected++;
+			}
+		}
+		return corrected;
 	}
 
+	private int correctBadValues(Config config, CorrectionListener listener, String configPath) {
+		int corrected = 0;
+		Set<Map.Entry<String, Object>> configEntries = config.asMap().entrySet();
+		for (Iterator<Map.Entry<String, Object>> it = configEntries.iterator(); it.hasNext(); ) {
+			Map.Entry<String, Object> configEntry = it.next();
+			String key = configEntry.getKey();
+			Object value = configEntry.getValue();
+
+			String path = (configPath.isEmpty()) ? key : configPath + '.' + key;
+			ValueSpecification spec = map.get(path);
+
+			if (value instanceof Config) {
+				corrected += correctBadValues(config, listener, path);//process the content of the config
+			} else {
+				if (spec == null) {//no specification -> remove the value
+					listener.onCorrect(CorrectionAction.REMOVE, path, value, null);
+					it.remove();
+					corrected++;
+				} else if (!spec.validator.test(value)) {//incorrect value -> replace by the default value
+					listener.onCorrect(CorrectionAction.REPLACE, path, value, spec.defaultValue);
+					configEntry.setValue(spec.defaultValue);
+					corrected++;
+				}
+			}
+		}
+		return corrected;
+	}
+
+	/**
+	 * Listens for corrections made by the methods {@link #correct(Config)} and
+	 * {@link #correct(Config, CorrectionListener)}.
+	 */
 	@FunctionalInterface
-	public interface CorrectionReaction {
+	public static interface CorrectionListener {
 		/**
-		 * Reacts to the correction of a config value.
+		 * Called when a config value is added, modified or removed by the correction.
 		 *
+		 * @param action         the action that was taken.
 		 * @param path           the path of the value.
 		 * @param incorrectValue the old, incorrect value. May be null if the value didn't exist before the
-		 *                       correction.
+		 *                       correction, <b>or if the value was actually null.</b>
 		 * @param correctedValue the new, corrected value. May be null if the value has been removed by the
-		 *                       correction.
+		 *                       correction, <b>or if the default value in the specification is null.</b>
 		 */
-		void onCorrect(String path, Object incorrectValue, Object correctedValue);
+		void onCorrect(CorrectionAction action, String path, Object incorrectValue, Object correctedValue);
+
+	}
+
+	public static enum CorrectionAction {
+		/**
+		 * Means that the value was added to the config. In that case, {@code incorrectValue} is {@code
+		 * null}.
+		 */
+		ADD,
+		/**
+		 * Means that the value was replaced. In that case, {@code incorrectValue} and/or {@code
+		 * correctedValue} <b>may be</b> {@code null}.
+		 */
+		REPLACE,
+		/**
+		 * Means that the value was removed from the config. In that case, {@code correctedValue} is
+		 * {@code null}.
+		 */
+		REMOVE
 	}
 
 	/**
